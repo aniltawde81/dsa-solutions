5c1b793b2c2357d403f2cd7131117cc0869c94d7
Anil Tawde <aniltawde81@gmail.com>
2026-06-13 10:02:11 +0530

    Create binary_search.py

---
+++ searching/binary_search.py
@@ -0,0 +1,16 @@
+arr = [10, 20, 30, 40, 50]
+target = 40
+
+low = 0
+high = len(arr) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == target:
+        print("Found at index", mid)
+        break
+    elif arr[mid] < target:
+        low = mid + 1
+    else:
+        high = mid - 1
