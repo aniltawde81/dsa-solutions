ee6937318603510b3a33ed3c11cee5f9201a250b
Anil Tawde <aniltawde81@gmail.com>
2026-06-14 15:37:23 +0530

    Create selection_sort.py

---
+++ sorting/selection_sort.py
@@ -0,0 +1,14 @@
+arr = [50, 20, 40, 10, 30]
+
+n = len(arr)
+
+for i in range(n):
+    min_index = i
+
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print(arr)
