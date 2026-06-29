10c76e20282d091fb0b12ce4ae88371746a1895b
Anil Tawde <aniltawde81@gmail.com>
2026-06-29 22:42:27 +0530

    Create two_sum.py

---
+++ hashing/two_sum.py
@@ -0,0 +1,13 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+seen = {}
+
+for i, num in enumerate(nums):
+    diff = target - num
+
+    if diff in seen:
+        print([seen[diff], i])
+        break
+
+    seen[num] = i
