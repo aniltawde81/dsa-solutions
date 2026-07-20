c761e0c8eb0490ed51d0ef9b3597fb962fc03bb2
Anil Tawde <aniltawde81@gmail.com>
2026-07-20 16:43:13 +0530

    Create 867_transpose_matrix.py

---
+++ leetcode/867_transpose_matrix.py
@@ -0,0 +1,18 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rows = len(matrix)
+cols = len(matrix[0])
+
+result = []
+
+for j in range(cols):
+    row = []
+    for i in range(rows):
+        row.append(matrix[i][j])
+    result.append(row)
+
+print(result)
