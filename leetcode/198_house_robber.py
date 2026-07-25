8c84ed2f87c84ac10d9a51c5b33aceb3fcaa3cb7
Anil Tawde <aniltawde81@gmail.com>
2026-07-25 16:53:55 +0530

    Create 198_house_robber.py

---
+++ leetcode/198_house_robber.py
@@ -0,0 +1,13 @@
+nums = [2, 7, 9, 3, 1]
+
+if len(nums) == 1:
+    print(nums[0])
+else:
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+    print(dp[-1])
